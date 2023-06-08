ce2f7384187413315108d42fb20ee2adc47332ea
Norbert <norbert.sosnowski.front@gmail.com>
2023-06-08 20:46:22 +0200

    Prepared final test file

--- test.js
+++ test.js
@@ -1,47 +1,41 @@
 import { RandomColorGenerator } from "./dist/rcc.js";
 
-// const button = document.querySelector(".submit-button");
-// const preview = document.querySelector(".color-preview");
-
-// button.addEventListener("click", e => {
-//   preview.styles.backgroundColor = RandomColorGenerator("rgb");
-// });
-
 // HSL    RGB     HEX
+
 console.log(
   RandomColorGenerator({
-    format: "RGB",
-    colorParts: ["220", "100", ""],
-    // alphaChannel: "0.12",
+    format: "HEX",
+    colorParts: ["", "", ""],
+    alphaChannel: "ff",
     optionsObj: {
       hsl: {
-        hue: { minValue: 100, maxValue: 360 },
-        saturation: { minValue: 60, maxValue: 80 },
-        lightness: { minValue: 80, maxValue: 100 },
-        opacity: {
-          // random: true,
-          minValue: 0.9,
-          maxValue: 1,
-        },
+        // hue: { minValue: 100, maxValue: 110 },
+        // saturation: { minValue: 60, maxValue: 80 },
+        // lightness: { minValue: 80, maxValue: 100 },
+        // opacity: {
+        // random: true,
+        // minValue: 0.9,
+        // maxValue: 1,
+        // },
       },
       hex: {
-        red: {
-          // minValue: 5,
-          // maxValue: 9,
-        },
-        green: {
-          // minValue: 0,
-          // maxValue: 5,
-        },
-        blue: {
-          // minValue: 2,
-          // maxValue: 3,
-        },
-        opacity: {
-          // random: true,
-          // minValue: 12,
-          // maxValue: 13,
-        },
+        // red: {
+        //   minValue: 5,
+        //   maxValue: 9,
+        // },
+        // green: {
+        //   minValue: 0,
+        //   maxValue: 5,
+        // },
+        // blue: {
+        //   minValue: 2,
+        //   maxValue: 3,
+        // },
+        // opacity: {
+        //   random: true,
+        //   minValue: 12,
+        //   maxValue: 13,
+        // },
         // minValue: 0,
         // maxValue: 14,
       },
@@ -49,6 +43,11 @@ console.log(
         // red: { minValue: 0, maxValue: 255 },
         // green: { minValue: 0, maxValue: 255 },
         // blue: { minValue: 0, maxValue: 255 },
+        // opacity: {
+        //   random: true,
+        //   minValue: 0.5,
+        //   maxValue: 0.6,
+        // },
       },
     },
   })
