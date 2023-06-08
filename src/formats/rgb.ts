b2b10181b3bb72ccc2e9e2ff5d36eb3c1b98aac1
Norbert <norbert.sosnowski.front@gmail.com>
2023-06-08 19:47:37 +0200

    Created rgb format

---
+++ src/formats/rgb.ts
@@ -0,0 +1,129 @@
+import {
+  RandomFormatArgs,
+  OptionChannelType as OptionChannelType,
+} from "../rccTypes.js";
+import { checkAlpha } from "../tools/checkAlpha.js";
+import { generateRandomFloatNumber } from "../tools/generateRandomFloatNumber.js";
+import { generateRandomNumber } from "../tools/generateRandomNumber.js";
+import keysObject from "../tools/keysObject.js";
+
+export default function rgb({
+  colorParts = ["", "", ""],
+  alphaChannel = undefined,
+  optionsObj: { rgb: options },
+}: RandomFormatArgs): string | null {
+  checkOptions();
+
+  const frame: string[] = [];
+  for (let i = 0; i < colorParts.length; i++) {
+    const userValue = colorParts[i];
+
+    if (typeof userValue === "string" && userValue.length > 0) {
+      frame.push(userValue);
+    } else {
+      switch (i) {
+        case 0:
+          frame.push(generateColorValue(options?.red));
+          break;
+        case 1:
+          frame.push(generateColorValue(options?.green));
+          break;
+        case 2:
+          frame.push(generateColorValue(options?.blue));
+          break;
+      }
+
+      function generateColorValue(colorOption: OptionChannelType): string {
+        const { minValue, maxValue } = colorOption ?? {
+          minValue: undefined,
+          maxValue: undefined,
+        };
+        if (minValue != null || maxValue != null) {
+          return passValues(minValue, maxValue);
+        } else if (options?.minValue != null || options?.maxValue != null) {
+          const { minValue, maxValue } = options;
+          return passValues(minValue, maxValue);
+        } else {
+          return generateRandomNumber(0, 255).toString();
+        }
+      }
+    }
+  }
+  const [red, green, blue] = frame;
+
+  function formatColor(
+    red: string,
+    green: string,
+    blue: string,
+    alpha?: number | string | null
+  ): string {
+    if (alpha != null) {
+      if (checkAlpha(alpha, "number")) {
+        return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+      }
+
+      alpha = options?.opacity?.random ? generateRandomFloatNumber(0, 1) : null;
+    } else if (options?.opacity) {
+      const { random, minValue, maxValue } = options.opacity;
+
+      if (minValue != null || maxValue != null) {
+        alpha = generateRandomFloatNumber(
+          minValue ? minValue : 0,
+          maxValue ? maxValue : 1
+        );
+      } else if (random) {
+        alpha = generateRandomFloatNumber(0, 1);
+      } else {
+        return `rgb(${red}, ${green}, ${blue})`;
+      }
+    }
+
+    return alpha != null
+      ? `rgba(${red}, ${green}, ${blue}, ${alpha})`
+      : `rgb(${red}, ${green}, ${blue})`;
+  }
+
+  return formatColor(red, green, blue, alphaChannel);
+
+  /*
+
+
+
+  checks
+
+
+
+  */
+
+  function checkOptions(): void {
+    if (options) {
+      keysObject(options).forEach(key => {
+        const property = options[key];
+        if (typeof property === "number") {
+          isWrongRGB(property, key);
+        }
+        if (typeof property === "object") {
+          keysObject(property).forEach(subKey => {
+            property[subKey] != null && isWrongRGB(property[subKey]!, key);
+          });
+        }
+      });
+    }
+  }
+}
+
+function isWrongRGB(numberToCheck: number, part: string): void {
+  const showError = () => {
+    throw new Error(`The ${part} is out of accepted range`);
+  };
+
+  if (part === "opacity") {
+    if (numberToCheck < 0 || numberToCheck > 1) showError();
+  }
+
+  if (numberToCheck < 0 || numberToCheck > 255) showError();
+}
+
+function passValues(min: number | undefined, max: number | undefined) {
+  return generateRandomNumber(min ? min : 0, max ? max : 255).toString();
+}
